ff1774b807e1396b35f4e00b6f7d820b9b85ad26
jamkerr <kerr0088@gmail.com>
2022-10-05 15:39:26 +0200

    Feat 1.9: Add feature to add item to list

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function newItem() {
+    //1. Adding a new item to the list of items: 
+    let list = $('#list');
+    let inputValue = $('#input').val();
+    let li = $(`<li>${inputValue}</li>`);
+    list.append(li);
+
+    //2. Crossing out an item from the list of items:
+
+    //3(i). Adding the delete button "X": 
+
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+
+    // 4. Reordering the items: 
+
+
+}
